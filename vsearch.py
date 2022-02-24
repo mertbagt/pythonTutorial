0d07bf94736ae736148179742333770e78bbcf3d
Merritt Baggett <mertbagt@gmail.com>
2022-02-24 16:21:50 -0500

    passing arguements

--- vsearch.py
+++ vsearch.py
@@ -1,7 +1,6 @@
-def search4vowels():
-        """Display any vowels found in an asked-for word."""
+def search4vowels(word):
+        """Display any vowels found in a supplied word."""
         vowels = set('aeiou')
-        word = input('Provide a word to search for vowels: ')
         found = vowels.intersection(set(word))
         for vowel in found:
                 print(vowel)
