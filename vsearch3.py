6134d1cccd2b4d5709fbfb1f51a56e5a9e904193
Merritt Baggett <mertbagt@gmail.com>
2022-03-01 15:55:55 -0500

    refactor

--- vsearch3.py
+++ vsearch3.py
@@ -1,5 +1,4 @@
 def search4vowels(word):
         """Return any vowels found in a supplied word."""
         vowels = set('aeiou')
-        found = vowels.intersection(set(word))
-        return found
+        return vowels.intersection(set(word))
