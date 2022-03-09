3fdf70f8a2b1e79d635661f20623b7f3c75ffe30
Merritt Baggett <mertbagt@gmail.com>
2022-03-09 15:22:28 -0500

    multiple argument functions

--- vsearch4.py
+++ vsearch4.py
@@ -1,4 +1,4 @@
 def search4vowels(phrase:str) -> set:
-        """Return any vowels found in a supplied word."""
+        """Return any vowels found in a 'phrase'."""
         vowels = set('aeiou')
         return vowels.intersection(set(phrase))
---
+++ vsearch5.py
@@ -0,0 +1,9 @@
+def search4vowels(phrase:str) -> set:
+        """Return any vowels found in a 'phrase'."""
+        vowels = set('aeiou')
+        return vowels.intersection(set(phrase))
+
+def search4letters(phrase:str, letters:str) -> set:
+        """Return a set of the 'letters' found in a 'phrase'."""
+
+        return set(letters).intersection(set(phrase))
