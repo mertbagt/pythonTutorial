ef3919e99452de60bf399d637a9b60fcbed36e1e
Merritt Baggett <mertbagt@gmail.com>
2022-11-07 14:12:48 -0500

    distribution module setup

---
+++ README.txt
@@ -0,0 +1,1 @@
+file for setup of distribution file for vsearch5.py
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup (
+  name='vsearch5.py',
+  version='1.0',
+  description='The Head First Python Search Tools',
+  author='HF Python 2e',
+  author_email='hfpy2e@gmail.com',
+  url='headfirstlabs.com',
+  py_modules=['vsearch'],
+)
