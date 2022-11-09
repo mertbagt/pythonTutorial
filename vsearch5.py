548c157f4ed79d9117a56ba5b4499f4f0c764469
Merritt Baggett <mertbagt@gmail.com>
2022-11-09 15:55:57 -0500

    pep compliance

--- vsearch5.py
+++ vsearch5.py
@@ -1,9 +1,9 @@
-def search4vowels(phrase:str) -> set:
+def search4vowels(phrase: str) -> set:
         """Return any vowels found in a 'phrase'."""
         vowels = set('aeiou')
         return vowels.intersection(set(phrase))
 
-def search4letters(phrase:str, letters:str='aeiou') -> set:
+def search4letters(phrase:str, letters: str='aeiou') -> set:
         """Return a set of the 'letters' found in a 'phrase'."""
 
         return set(letters).intersection(set(phrase))
