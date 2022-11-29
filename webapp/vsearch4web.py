1623c6240ce5f28f94bc8ba571459d1b4a2bcdf6
Merritt Baggett <mertbagt@gmail.com>
2022-11-29 11:04:32 -0500

    post method

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello() -> str:
     return 'Hello world from Flask!'
 
-@app.route('/search4')
+@app.route('/search4', methods=['POST'])
 def do_search() -> str:
     return str(search4letters('life, the universe, and everything', 'eiru,!'))
 
@@ -16,4 +16,4 @@ def entry_page() -> 'html':
     return render_template('entry.html',
                             the_title='Welcome to search4letters on the web')
     
-app.run()
+app.run(debug=True)
